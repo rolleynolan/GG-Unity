ca15aa4f64987bd2a1f617e8ecad82a403c95306
Nolan <normaldude54@gmail.com>
2025-06-18 15:02:58 -0500

    Rename 'conference' to 'teamConference' in TeamID class for clarity and consistency; add metadata file for TeamDatabase.

--- Assets/Scripts/TeamData.cs
+++ Assets/Scripts/TeamData.cs
@@ -6,7 +6,7 @@ public class TeamID
     public string city;
     public string name;
     public string abbreviation;
-    public string conference;
+    public string teamConference;
     public string division;
     public string id;
 }
