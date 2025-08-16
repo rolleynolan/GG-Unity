33d59cf3f2f02fe37e4a25d3b0369b02c2c7695d
Nolan <normaldude54@gmail.com>
2025-08-16 00:02:42 -0500

    Replace core path utilities with infra and update logging

--- Assets/Scripts/Core/GGLog.cs
+++ Assets/Scripts/Core/GGLog.cs
@@ -1,24 +1,7 @@
-using System.IO;
-using UnityEngine;
+using GG.Infra;
 
 public static class GGLog {
-  public static bool Verbose =
-#if GG_VERBOSE_LOGS
-    true;
-#else
-    false;
-#endif
-
-  static string logFile;
-  static string LogFile { get { if (logFile==null) logFile = GGPaths.Save("gg_log.txt"); return logFile; } }
-
-  static void Write(string level, string msg){
-#if !UNITY_EDITOR
-    try { File.AppendAllText(LogFile, $"[{level}] {msg}\n"); } catch { }
-#endif
-  }
-
-  public static void Info(string m){ if (Verbose) Debug.Log($"[GG] {m}"); Write("I", m); }
-  public static void Warn(string m){ Debug.LogWarning($"[GG] {m}"); Write("W", m); }
-  public static void Error(string m){ Debug.LogError($"[GG] {m}"); Write("E", m); }
+  public static void Info(string m)  => GG.Infra.GGLog.Info(m);
+  public static void Warn(string m)  => GG.Infra.GGLog.Warn(m);
+  public static void Error(string m) => GG.Infra.GGLog.Error(m);
 }
